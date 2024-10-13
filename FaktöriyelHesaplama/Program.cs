de24776607b0fb46d200c856a5b96a38f0b15241
evinyanitoz <eyanitoz@gmail.com>
2024-10-13 15:19:04 +0300

    Add project files.

---
+++ FaktöriyelHesaplama/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace FaktöriyelHesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("KAÇ FAKTÖRİYEL");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 1;
+
+            for (int i = 1; i <= sayi; i++)
+            {
+
+                toplam *= i;  
+            }
+
+            Console.WriteLine(toplam);
+
+        }
+
+    }
+}
